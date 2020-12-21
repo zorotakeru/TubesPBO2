1182623dfa9067b4fb198871b57da6cba7d15216
zorotakeru <helmy18.hhh@gmail.com>
2020-12-21 14:52:33 +0700

    Show Characters

--- src/DAO/DaoCharacter.java
+++ src/DAO/DaoCharacter.java
@@ -14,19 +14,22 @@ import java.util.List;
 
 public class DaoCharacter implements DaoInterface{
     @Override
-    public ObservableList<Characters> showData() {
+    public ObservableList showData() {
         ObservableList<Characters> chList = FXCollections.observableArrayList();
         try {
-            String query = "SELECT * FROM user";
+            String query = "SELECT user.idUser,user.nameUser,user.Point,user.Level,COUNT(monster.idMonster) AS 'totalMonster' FROM user JOIN monster WHERE user.idUser = monster.User_idUser";
             PreparedStatement statement;
             statement = JDBCConnection.getConnection().prepareStatement(query);
             ResultSet result= statement.executeQuery();
             while (result.next()){
-                int idChar = result.getInt("idChar");
-                String nameUser = result.getString("colCharName");
-                int Point = result.getInt("Point");
-                int Level = result.getInt("colCharLevel");
-                Characters cha = new Characters(idChar,nameUser, Point,Level);
+                int idChar = result.getInt("user.idUser");
+                String nameUser = result.getString("user.nameUser");
+                int point = result.getInt("user.Point");
+                int level = result.getInt("user.Level");
+                int mCount = result.getInt("totalMonster");
+                System.out.println(mCount);
+                Characters ch = new Characters(nameUser,point,level,mCount);
+                Characters cha = new Characters(idChar,ch);
                 chList.add(cha);
             }
         }
